329fdb40f7dc5b534b7fa43789daef79f9140f1f
Tomas Garbarino <tomydevtl@gmail.com>
2023-04-11 01:32:24 -0300

    Update styles and improve Loading Screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/NavBar/NavBar.jsx'
-import LoadingScreen from './components/LoadingScreen/LoadingScreen.jsx'
-import ClickableComponent from './components/DraggableModal/ClickableComponent.jsx';
+import Navbar from './components/NavBar/NavBar'
+import LoadingScreen from './components/LoadingScreen/LoadingScreen'
+import ClickableComponent from './components/DraggableModal/ClickableComponent';
 
 
 
--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import './LoadingScreen.css';
 
 function LoadingScreen() {
+  const currentYear = new Date().getFullYear();
+
   useEffect(() => {
     const timer = setTimeout(() => {
       const loadingScreen = document.getElementById("loading-screen");
       loadingScreen.remove();
-    }, 6000);
+    }, 4500);
 
     return () => {
       clearTimeout(timer);
@@ -15,9 +17,27 @@ function LoadingScreen() {
 
   return (
     <div id="loading-screen" className="loading-screen">
-      <div className="loading-text">IBM Personal Computer Basic Version 1.0</div>
-      <div className="loading-text">Copyright IBM Corp 1981, 1983</div>
-      <div className="login-prompt">Welcome</div>
+      <div className="loading-text">
+        {Array.from("Garba Personal Computer Basic Version 1.0").map((letter, index) => (
+          <span key={index} data-text={letter}>
+            {letter === ' ' ? '\u00A0' : letter}
+          </span>
+        ))}
+      </div>
+      <div className="loading-text">
+        {Array.from(`Copyright Garba Corp 2000, ${currentYear}`).map((letter, index) => (
+          <span key={index} data-text={letter}>
+            {letter === ' ' ? '\u00A0' : letter}
+          </span>
+        ))}
+      </div>
+      <div className="login-prompt">
+        {Array.from("Welcome").map((letter, index) => (
+          <span key={index} data-text={letter}>
+            {letter === ' ' ? '\u00A0' : letter}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
